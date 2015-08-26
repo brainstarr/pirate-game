1190dcedffbbf07b5dbc8b2546d08b3a4649d6ec
Brian Starr <brian.r.starr@gmail.com>
2015-08-26 11:55:46 -0700

    changed a comment to test circle CI, build is broken for testing purposes

--- Pirate Challenge/BSAppDelegate.h
+++ Pirate Challenge/BSAppDelegate.h
@@ -5,8 +5,10 @@
 //  Created by Brian Starr on 9/4/14.
 //  Copyright (c) 2014 Brian Starr. All rights reserved.
 //
+// change for testing circle
 
 #import <UIKit/UIKit.h>
+
 
 @interface BSAppDelegate : UIResponder <UIApplicationDelegate>
 
